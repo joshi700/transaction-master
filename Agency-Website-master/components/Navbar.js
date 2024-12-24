603911b0dc3104409c0349efaff05a9a328df613
GJ <joshi700@gmail.com>
2024-12-24 17:42:04 -0500

    Update Navbar.js

--- Agency-Website-master/components/Navbar.js
+++ Agency-Website-master/components/Navbar.js
@@ -7,7 +7,7 @@ function Navbar() {
   <a class="navbar-brand me-5" href="#">
     <img
       class="BrandLogo"
-      src="./Images/Transaction.png"
+      src="./Images/Transaction 2.png"
       alt=""
       srcset=""
     />
