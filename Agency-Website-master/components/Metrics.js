e094e132d96c384ccfa85ba69a5a26373c0f397b
GJ <joshi700@gmail.com>
2024-12-25 11:04:36 -0500

    Update Metrics.js

--- Agency-Website-master/components/Metrics.js
+++ Agency-Website-master/components/Metrics.js
@@ -9,7 +9,7 @@ function Metrics() {
       <!-- Team Member 1 -->
       <div class="col-12 col-md-4 text-center">
         <div class="team-member">
-          <img src="/Images/john-doe-circle.webp" alt="Member 1" class="team-image">
+          <img src="/Images/Saylee.jpeg" alt="Member 1" class="team-image">
           <h3>Saylee Mokashi</h3>
           <p>CEO & Transaction Manager</p>
           <p>Saylee is the go-to person who helps the clients to guide them through the often-emotional process of selling their beloved home and/or the exciting process of buying their dream home. With a background in hospitality, Saylee truly understands the importance of providing outstanding customer service.
@@ -21,7 +21,7 @@ After having lived in four countries over the past several years, Saylee and her
       <!-- Team Member 2 -->
       <div class="col-12 col-md-4 text-center">
         <div class="team-member">
-          <img src="/Images/john-doe-circle.webp" alt="Member 2" class="team-image">
+          <img src="/Images/Shraddha.jpeg" alt="Member 2" class="team-image">
           <h3>Shraddha Kharsikar</h3>
           <p>Transaction Coordinator</p>
           <p>Coming from a Human Resources background, Shraddha brings in her experience of excellent organisation skills, time management and multitasking skills. She deeply understands that strong and incessant communication is the key to any successful relationship.Buying and Selling home is an extremely emotional experience for every person which needs to be handled with personal touch. Shraddha firmly believes in "what goes around, comes around" and hence she gives the best to our clients. 
