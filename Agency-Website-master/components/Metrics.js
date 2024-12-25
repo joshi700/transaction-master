847af384cd9f99777275df29ab08da5f0f86fc51
GJ <joshi700@gmail.com>
2024-12-25 10:56:08 -0500

    Update Metrics.js

--- Agency-Website-master/components/Metrics.js
+++ Agency-Website-master/components/Metrics.js
@@ -11,8 +11,10 @@ function Metrics() {
         <div class="team-member">
           <img src="/Images/john-doe-circle.webp" alt="Member 1" class="team-image">
           <h3>Saylee Mokashi</h3>
-          <p>CEO & Founder</p>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin quis nisi sit amet urna iaculis mollis.</p>
+          <p>CEO & Transaction Manager</p>
+          <p>Saylee is the go-to person who helps the clients to guide them through the often-emotional process of selling their beloved home and/or the exciting process of buying their dream home. With a background in hospitality, Saylee truly understands the importance of providing outstanding customer service.
+
+After having lived in four countries over the past several years, Saylee and her family have chosen beautiful South East Michigan to be their forever home. When she is not working, Saylee volunteers as an English language tutor and loves to spend quality time with her husband and young daughter.</p>
         </div>
       </div>
 
@@ -20,9 +22,10 @@ function Metrics() {
       <div class="col-12 col-md-4 text-center">
         <div class="team-member">
           <img src="/Images/john-doe-circle.webp" alt="Member 2" class="team-image">
-          <h3>Shraddha</h3>
-          <p>Chief Marketing Officer</p>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin quis nisi sit amet urna iaculis mollis.</p>
+          <h3>Shraddha Kharsikar</h3>
+          <p>Transaction Coordinator</p>
+          <p>Coming from a Human Resources background, Shraddha brings in her experience of excellent organisation skills, time management and multitasking skills. She deeply understands that strong and incessant communication is the key to any successful relationship.Buying and Selling home is an extremely emotional experience for every person which needs to be handled with personal touch. Shraddha firmly believes in "what goes around, comes around" and hence she gives the best to our clients. 
+Shraddha is gregarious and loves to socialize. In her spare time, she likes to try out new recipes and enjoys bollywood dancing. From last seven years, she lives in Canton with her loving husband and young son. </p>
         </div>
       </div>
 
