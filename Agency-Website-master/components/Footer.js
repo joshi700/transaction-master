e2bc1f1522f4afc985b861a76e0b904c9f85b9e7
GJ <joshi700@gmail.com>
2024-12-25 07:17:41 -0500

    Update Footer.js

--- Agency-Website-master/components/Footer.js
+++ Agency-Website-master/components/Footer.js
@@ -3,17 +3,28 @@ function Footer() {
   const Container = document.createElement("div");
   Container.classList.add("container");
   Container.innerHTML = `
-  <div class="row gx-4 gy-5">
-          <div class="col-12 col-md-10 left-section ">
-            <div class="copyrights mt-4">
-              <H2>Contact Us:</H2>
-            <div class="badges-items me-3 d-block">
-              <H3>248-716-4247</H3>
-              <H3>transaction.management.one@gmail.com</H3>
-            </div>
+    <div class="row gx-4 gy-5">
+      <div class="col-12 col-md-6 left-section">
+        <h2>Contact Us:</h2>
+        <div class="contact-items d-flex flex-column flex-sm-row align-items-center">
+          <div class="contact-item me-4">
+            <i class="bi bi-telephone-fill"></i>
+            <a href="tel:+12487164247" class="contact-link">248-716-4247</a>
+          </div>
+          <div class="contact-item">
+            <i class="bi bi-envelope-fill"></i>
+            <a href="mailto:transaction.management.one@gmail.com" class="contact-link">transaction.management.one@gmail.com</a>
           </div>
         </div>
+      </div>
+      <div class="col-12 col-md-6 text-center text-md-end">
+        <p>&copy; 2024 Your Company Name. All rights reserved.</p>
+      </div>
+    </div>
   `;
+
   Footer.appendChild(Container);
 }
+
+// Call the function to render the footer
 Footer();
