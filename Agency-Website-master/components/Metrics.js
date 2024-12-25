fe82719e1dae94ae72af5ee20dcfe2e876332347
GJ <joshi700@gmail.com>
2024-12-25 11:11:52 -0500

    Update Metrics.js

--- Agency-Website-master/components/Metrics.js
+++ Agency-Website-master/components/Metrics.js
@@ -12,7 +12,7 @@ function Metrics() {
           <img src="/Images/Saylee.jpeg" alt="Member 1" class="team-image">
           <h3>Saylee Mokashi</h3>
           <p>CEO</p>
-          <p>Saylee Mokashi is the go-to expert in guiding clients through the often-emotional journey of selling their home or the exciting process of buying their dream home. With a background in hospitality, she excels in providing exceptional customer service. Having lived in four countries, Saylee and her family have made South East Michigan their forever home. Outside of work, she enjoys volunteering as an English language tutor and spending quality time with her husband and young daughter.</p>
+          <p>Saylee Mokashi is the go-to expert in guiding clients through the often-emotional journey of selling their home or the exciting process of buying their dream home. With a background in hospitality, she excels in providing exceptional customer service. </p>
         </div>
       </div>
 
@@ -22,7 +22,7 @@ function Metrics() {
           <img src="/Images/Shraddha.jpg" alt="Member 2" class="team-image">
           <h3>Shraddha Kharsikar</h3>
           <p>Transaction Manager</p>
-          <p>Shraddha leverages her Human Resources background to bring exceptional organizational skills, time management, and communication to every real estate transaction. She understands the emotional complexities of buying and selling homes and ensures each process is handled with care and attention. Committed to delivering the best for clients, Shraddha’s approach is rooted in building strong, lasting relationships. Outside of work, she enjoys socializing, cooking, and Bollywood dancing, and has lived in Canton with her family for seven years.</p>
+          <p>Shraddha leverages her Human Resources background to bring exceptional organizational skills, time management, and communication to every real estate transaction. She understands the emotional complexities of buying and selling homes and ensures each process is handled with care and attention. </p>
         </div>
       </div>
 
