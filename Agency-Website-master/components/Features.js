fc6fb39a59fa836b7508463caceb11675ffce97d
GJ <joshi700@gmail.com>
2024-12-27 07:59:59 -0500

    Update Features.js

--- Agency-Website-master/components/Features.js
+++ Agency-Website-master/components/Features.js
@@ -6,11 +6,11 @@ function Features() {
   <div
   class="text-body text-center d-flex flex-column align-items-center"
 >
-  <h2 class="title fw-bold">Tailor-made features</h2>
-  <p class="">
+  <h2 class="title fw-bold">Key Features</h2>
+  <!-- <p class="">
     Lorem ipsum is common placeholder text used to demonstrate the
     graphic elements of a document or visual presentation.
-  </p>
+  </p> -->
 </div>
 <div
   class="container-body row row-cols-1 row-cols-md-2 row-cols-lg-3 gy-4"
@@ -20,10 +20,9 @@ function Features() {
       <img src="./Images/Base-feature-icon-1.svg" alt="" srcset="" />
     </div>
     <div class="text">
-      <div class="title fw-bold text-center">Robust workflow</div>
+      <div class="title fw-bold text-center">Reliable & Transparent Support</div>
       <div class="description text-center">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-        erat nibh tristique ipsum.
+        Round-the-clock assistance with clear, consistent communication.
       </div>
     </div>
   </div>
@@ -32,10 +31,9 @@ function Features() {
       <img src="./Images/Base-feature-icon-3.svg" alt="" srcset="" />
     </div>
     <div class="text">
-      <div class="title fw-bold text-center">Flexibility</div>
+      <div class="title fw-bold text-center">Skilled & Detail-Oriented</div>
       <div class="description text-center">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-        erat nibh tristique ipsum.
+        Professional management of timelines and processes for seamless real estate transactions.
       </div>
     </div>
   </div>
@@ -44,10 +42,9 @@ function Features() {
       <img src="./Images/Base-feature-icon-5.svg" alt="" srcset="" />
     </div>
     <div class="text">
-      <div class="title fw-bold text-center">User friendly</div>
+      <div class="title fw-bold text-center">Competitive & Affordable Rates</div>
       <div class="description text-center">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-        erat nibh tristique ipsum.
+        Offering cost-effective solutions without compromising on quality.
       </div>
     </div>
   </div>
@@ -56,10 +53,9 @@ function Features() {
       <img src="./Images/Base-feature-icon-7.svg" alt="" srcset="" />
     </div>
     <div class="text">
-      <div class="title fw-bold text-center">Multiple layouts</div>
+      <div class="title fw-bold text-center">Financial Systems Management</div>
       <div class="description text-center">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-        erat nibh tristique ipsum.
+        Comprehensive support for agent budgets and financial operations.
       </div>
     </div>
   </div>
@@ -68,10 +64,9 @@ function Features() {
       <img src="./Images/Base-feature-icon-2.svg" alt="" srcset="" />
     </div>
     <div class="text">
-      <div class="title fw-bold text-center">Better components</div>
+      <div class="title fw-bold text-center">Cost Savings</div>
       <div class="description text-center">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-        erat nibh tristique ipsum.
+        More affordable than in-house transaction management, without the overhead of training and recruiting.
       </div>
     </div>
   </div>
@@ -80,10 +75,9 @@ function Features() {
       <img src="./Images/Base-feature-icon-8.svg" alt="" srcset="" />
     </div>
     <div class="text">
-      <div class="title fw-bold text-center">Well organised</div>
+      <div class="title fw-bold text-center">Industry Standard Practices</div>
       <div class="description text-center">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-        erat nibh tristique ipsum.
+        Utilizing best practices while adapting to the unique needs of each transaction.
       </div>
     </div>
   </div>
